0d9edd35458940ddebab24fa4ec7f594423c26bb
swaran21 <saiswaran212005@gmail.com>
2025-04-14 21:51:00 +0530

    addes jwt token class

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -2,6 +2,7 @@ package com.pm.authservice.service;
 
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.model.User;
+import com.pm.authservice.util.JwtUtil;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +13,12 @@ public class AuthService {
 
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public AuthService(UserService userService, PasswordEncoder passwordEncoder) {
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     public Optional<String> authenticate (LoginRequestDTO loginRequestDTO) {
