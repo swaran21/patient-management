a0855244ee857f6c0c1fcd6fe2166c70206e8af6
swaran21 <saiswaran212005@gmail.com>
2025-04-14 21:12:55 +0530

    auth service and user service without the jwt and passwordencoder

---
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -0,0 +1,26 @@
+package com.pm.authservice.service;
+
+import com.pm.authservice.dto.LoginRequestDTO;
+import com.pm.authservice.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    private final UserService userService;
+
+    public AuthService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public Optional<String> authenticate (LoginRequestDTO loginRequestDTO) {
+        Optional<String> token = userService
+                .findByEmail(loginRequestDTO.getEmail())
+                .filter(u->passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        u.getPassword()))
+                .map(u->jwtUtil.generateToken(u.getEmail(),u.getRole()));
+        return token;
+    }
+}
