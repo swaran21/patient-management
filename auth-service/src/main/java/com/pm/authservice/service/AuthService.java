2bf592e720091c1257ce60ca96ecfa14bc223dbb
swaran21 <saiswaran212005@gmail.com>
2025-04-15 15:44:11 +0530

    validate token and http api

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -1,8 +1,8 @@
 package com.pm.authservice.service;
 
 import com.pm.authservice.dto.LoginRequestDTO;
-import com.pm.authservice.model.User;
 import com.pm.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -29,4 +29,15 @@ public class AuthService {
                 .map(u->jwtUtil.generateToken(u.getEmail(),u.getRole()));
         return token;
     }
+
+    public boolean validateToken(String token) {
+        try{
+            jwtUtil.validateToken(token);
+            return true;
+        }
+        catch(JwtException e){
+            return false;
+        }
+    }
+
 }
