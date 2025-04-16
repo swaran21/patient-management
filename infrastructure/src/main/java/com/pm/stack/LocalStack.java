8130261aecae42a748c532875c88a00cbdcfb0a2
swaran21 <saiswaran212005@gmail.com>
2025-04-16 19:03:49 +0530

    created msk cluster

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,12 +1,13 @@
 package com.pm.stack;
 
 import software.amazon.awscdk.*;
-import software.amazon.awscdk.services.ec2.InstanceClass;
-import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ec2.InstanceType;
-import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.msk.CfnCluster;
 import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.route53.CfnHealthCheck;
+
+import java.util.stream.Collectors;
 
 public class LocalStack extends Stack {
 
@@ -22,6 +23,7 @@ public class LocalStack extends Stack {
 
         CfnHealthCheck authDbHealthCheck = createDbHealthCheck(authServiceDb,"AuthServiceDbHealthCheck");
         CfnHealthCheck patientDbHealthCheck = createDbHealthCheck(patientServiceDb,"PatientServiceDbHealthCheck");
+        CfnCluster mskCluster = createMskCluster();
 
     }
 
@@ -45,6 +47,17 @@ public class LocalStack extends Stack {
                 .databaseName(dbName)
                 .removalPolicy(RemovalPolicy.DESTROY)
                 .build();
+    }
+
+    private CfnCluster createMskCluster() {
+        return CfnCluster.Builder.create(this,"MskCluster")
+                .clusterName("kafka-cluster")
+                .kafkaVersion("2.8.0")
+                .numberOfBrokerNodes(1)
+                .brokerNodeGroupInfo(CfnCluster.BrokerNodeGroupInfoProperty.builder()
+                        .instanceType("kafka.m5.xlarge")
+                        .clientSubnets(vpc.getPrivateSubnets().stream().map(ISubnet::getSubnetId).collect(Collectors.toList()))
+                        .brokerAzDistribution("DEFAULT").build()).build();
     }
 
     private CfnHealthCheck createDbHealthCheck(DatabaseInstance db,String id) {
