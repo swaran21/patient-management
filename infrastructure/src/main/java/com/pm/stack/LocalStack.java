5c6c530229f60dcbafd4a95a53785c0b3b1fc590
swaran21 <saiswaran212005@gmail.com>
2025-04-16 18:08:06 +0530

    create vpc

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,11 +1,22 @@
 package com.pm.stack;
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.Vpc;
 
 public class LocalStack extends Stack {
 
+    private final Vpc vpc;
+
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
+        this.vpc = createVpc();
+    }
+
+    private Vpc createVpc() {
+        return Vpc.Builder.create(this,"PatientManagementVpc")
+                .vpcName("PatientManagementVPC")
+                .maxAzs(2)
+                .build();
     }
 
     public static void main(final String[] args) {
